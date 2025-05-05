d8b581239e715738d1bdb0fb2ce8b86e27ecd19c
Alexandr <alpinefuchs@gmail.com>
2025-05-06 01:22:50 +0300

    add insertStudyGroup

--- server/src/main/java/nihvostain/managers/DataBasesManager.java
+++ server/src/main/java/nihvostain/managers/DataBasesManager.java
@@ -1,5 +1,7 @@
 package nihvostain.managers;
 
+import common.model.StudyGroup;
+
 import java.sql.*;
 
 public class DataBasesManager {
@@ -36,5 +38,43 @@ public class DataBasesManager {
         statement.executeUpdate();
     }
 
+    public void insertStudyGroup (String key, StudyGroup studyGroup, String login) throws SQLException {
+        String sql = "insert into StudyGroups (key, name, x, y, creationDate, studentsCount," +
+                " formOfEducation, semesterEnum, nameP, birthday, passportID, eyeColor, hairColor, login)" +
+                "  values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, key);
+        statement.setString(2, studyGroup.getName());
+        statement.setLong(3, studyGroup.getCoordinates().getX());
+        statement.setFloat(4, studyGroup.getCoordinates().getY());
+        statement.setTimestamp(5, Timestamp.valueOf(studyGroup.getCreationDate()));
+        statement.setLong(6, studyGroup.getStudentsCount());
+        statement.setString(7, studyGroup.getFormOfEducation().toString());
+        statement.setString(8, studyGroup.getSemesterEnum().toString());
+        if (studyGroup.getGroupAdmin() != null) {
+            statement.setString(9, studyGroup.getGroupAdmin().getName());
+            if (studyGroup.getGroupAdmin().getBirthday() != null) {
+                statement.setString(10, studyGroup.getGroupAdmin().getBirthday().toString());
+            } else {
+                statement.setString(10, null);
+            }
+            statement.setString(11, studyGroup.getGroupAdmin().getPassportID());
+            if (studyGroup.getGroupAdmin().getEyeColor() != null) {
+                statement.setString(12, studyGroup.getGroupAdmin().getEyeColor().toString());
+            } else {
+                statement.setString(12, null);
+            }
+            statement.setString(13, studyGroup.getGroupAdmin().getHairColor().toString());
+        } else {
+            statement.setString(9, null);
+            statement.setString(10, null);
+            statement.setString(11, null);
+            statement.setString(12, null);
+            statement.setString(13, null);
+        }
+        statement.setString(14, login);
+        statement.executeUpdate();
+
+    }
 
 }
