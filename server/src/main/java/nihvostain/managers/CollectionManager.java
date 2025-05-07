6aa27d3de4cb8f08d63ff4512b05fbe9536bce18
Alexandr <alpinefuchs@gmail.com>
2025-05-07 11:07:56 +0300

    loadDb

--- server/src/main/java/nihvostain/managers/CollectionManager.java
+++ server/src/main/java/nihvostain/managers/CollectionManager.java
@@ -78,6 +78,9 @@ public class CollectionManager {  //receiver
         }
     }
 
+    public void loadDb(DataBasesManager dataBasesManager) throws SQLException {
+        studyGroupList = dataBasesManager.parseStudyGroups();
+    }
     /**
      * информация о коллекции
      */
