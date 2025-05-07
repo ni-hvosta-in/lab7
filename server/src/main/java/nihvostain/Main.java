4b9c2449f9063cfdc31ef5fcb4c648350d1614af
Alexandr <alpinefuchs@gmail.com>
2025-05-07 11:08:07 +0300

    loadDb

--- server/src/main/java/nihvostain/Main.java
+++ server/src/main/java/nihvostain/Main.java
@@ -21,7 +21,7 @@ public class Main {
         CollectionManager collectionManager = new CollectionManager();
         DataBasesManager dataBasesManager = new DataBasesManager("jdbc:postgresql://localhost:5432/postgres", "postgres", "1");
         dataBasesManager.parseStudyGroups();
-        collectionManager.load(System.getenv("MY_VAR"));
+        collectionManager.loadDb(dataBasesManager);
         int serverPort = 9898;
         int bufferCapacity = 10000;
         Communication communication = new Communication(serverPort, bufferCapacity);
